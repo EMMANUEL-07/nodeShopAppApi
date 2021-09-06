09ca4f56a70171d9204270580aef24c5b66b817c
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2021-09-06 02:22:32 +0100

    using graphql beginning

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const express = require("express")
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const multer = require('multer')
+const axios = require('axios')
+const graphql = require('express-graphql')
 
 require('dotenv').config()
 
-const feedRoutes = require('./routes/feed')
-const authRoutes = require('./routes/auth')
+const graphqlSchema = require('./graphql/schema')
+const graphqlResolver = require('./graphql/resolvers')
+
+const auth = require('./middleware/auth')
 
 const app = express()
 
@@ -29,20 +33,20 @@ const fileStorage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
    if (
-     file.mimetype === 'image/png' ||
-     file.mimetype === 'image/jpg' ||
-     file.mimetype === 'image/jpeg'
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
    ) {
-     cb(null, true);
+      cb(null, true);
    } else {
-     cb(null, false);
+      cb(null, false);
    }
 };
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-   multer({ storage: fileStorage, fileFilter: fileFilter}).single('image')
+   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 )
 app.use('/images', express.static(path.join(__dirname, 'images'))); // static rendering for images
 
@@ -52,33 +56,65 @@ app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
    // res.setHeader('Access-Control-Allow-Headers', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+  }
 
    next();
 
 })
 
+/* 
 app.use('/feed', feedRoutes)
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes) */
+
+app.put('/post-image', (req, res, next) => {
+   if(!req.file){
+      return res.status(200).json({message: 'No file provided'})
+   }
+})
+
+app.use(auth)
+
+app.use('/graphql', graphql.graphqlHTTP({
+   schema: graphqlSchema,
+   rootValue: graphqlResolver,
+   graphiql: true,
+   formatError(err) {
+      if(!err.originalError) {
+         return err;
+      }
+
+      const data = err.originalError.data;
+      const message = err.message || 'An error occured'
+      const code = err.originalError.code || 500;
+
+      return { message: message, status : code, data: data}
+   }
+}))
 
 app.use((error, req, res, next) => {
    console.log(error)
    const status = error.statusCode || 500;
    const message = error.message
 
-   res.status(status).json({message:message})
+   res.status(status).json({ message: message })
 
 })
 
 mongoose
    .connect(MONGODB_URI)
    .then(result => {
-      /* const server = app.listen(PORT || 8080) */
+      app.listen(PORT || 8080, () => {
+         console.log("Application started and Listening on port 3000")
+      })
+      /* const server = app.listen(PORT || 8080) 
       const server = app.listen(8080)
       const io = require('./socket').init(server);
 
       io.on('connection', socket => {
          console.log('Client Connected')
-      })
+      })*/
    })
    .catch(err => {
       console.log(err);
