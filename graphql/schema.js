a9ac34b0ad9fa89c8c7a1012acf80d366fed4f7d
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2021-09-06 20:40:55 +0100

    done with graphql

--- graphql/schema.js
+++ graphql/schema.js
@@ -46,11 +46,16 @@ module.exports = buildSchema(`
    type RootQuery {
       login(email: String!, password: String!): AuthData!
       posts(page: Int): PostData!
+      post(id: ID!): Post!
+      user: User!
    }
 
    type RootMutation {
       createUser(userInput: userInputData): User!
       createPost(postInput: postInputData) : Post!
+      updatePost(id: ID!, postInput: postInputData): Post!
+      deletePost(id: ID!): Boolean
+      updateStatus(status: String!): User!
    }   
 
    schema {
